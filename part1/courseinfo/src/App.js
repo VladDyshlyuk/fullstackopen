e013925c3684e2c4d0d6bf955d5171dda76b1353
VladDyshlyuk <vladdyshlyuk@yadnex.ru>
2023-02-12 11:02:17 +0700

    courseinfo modified

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,7 +2,7 @@
 const App = () => {
   const course =
   {
-    title: 'Half Stack application development',
+    name: 'Half Stack application development',
     parts: [
       { name: 'Fundamentals of React', exercises: 10 },
       { name: 'Using props to pass data', exercises: 7 },
@@ -20,7 +20,7 @@ const App = () => {
 
 const Header = (props) => {
   return (
-    <h1>{props.course.title}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
